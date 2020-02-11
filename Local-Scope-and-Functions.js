7e8168627ed4f4c6cf131903f1d791d9bc96309c
javiermayan <44719141+javiermayan@users.noreply.github.com>
2020-02-11 02:48:12 -0300

    Create Local-Scope-and-Functions.js

---
+++ Local-Scope-and-Functions.js
@@ -0,0 +1,12 @@
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+  var myVar; 
+  console.log(myVar); 
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log(myVar); //outputs: "ReferenceError: myVar is not defined"
+
+// Now remove the console log line to pass the test
