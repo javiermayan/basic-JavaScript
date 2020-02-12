d40f76c3d0b2a2660a2d50ac1f512dcb6982ec35
javiermayan <44719141+javiermayan@users.noreply.github.com>
2020-02-12 03:26:34 -0300

    Create Return-a-Value-from-a-Function with-Return.js

---
+++ Return-a-Value-from-a-Function with-Return.js
@@ -0,0 +1,14 @@
+//Return a Value from a Function with Return
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(numero){
+return numero * 5;
+}
+
+console.log(minusSeven(10));
+console.log(timesFive(3));
